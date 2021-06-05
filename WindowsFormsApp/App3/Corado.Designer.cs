39c291bc431949fb49b519c3194ea9258d47bc19
Quang Truong ✔️ <truongnq2k@gmail.com>
2021-06-05 15:54:11 +0700

    delete note.png and new project

---
+++ WindowsFormsApp/App3/Corado.Designer.cs
@@ -0,0 +1,50 @@
+﻿
+namespace App3
+{
+    partial class Corado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Corado));
+            this.SuspendLayout();
+            // 
+            // Corado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Corado";
+            this.Text = "CoraDo - Todo Pro";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
